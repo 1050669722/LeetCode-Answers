21d25b0fe5485c80c72b4e1beecc43b092dcf929
1050669722 <42562697+1050669722@users.noreply.github.com>
2020-10-02 11:30:51 +0800

    Update problem0771.py

--- Python/problem0771.py
+++ Python/problem0771.py
@@ -5,38 +5,55 @@ Created on Sat May 25 10:32:55 2019
 @author: Administrator
 """
 
-import time
+# import time
 
-time1 = time.perf_counter()
+# time1 = time.perf_counter()
+
+# class Solution:
+#     def numJewelsInStones(self, J: str, S: str) -> int:
+# #        if J == '' or S == '':
+# #            return 0
+# #        else:
+# #            count = 0
+# #            for p in J:
+# #                for q in S:
+# #                    if p == q:
+# #                        count += 1
+# #        return count
+#         if J == '' or S == '':
+#             return 0
+#         else:
+#             d = {}
+#             count = 0
+#             for p in S:
+#                 try:
+#                     d[p] += 1
+#                 except:
+#                     d[p] = 1
+#             for q in J:
+#                 try:
+#                     count += d[q]
+#                 except:
+#                     pass
+#         return count
+
 
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
-#        if J == '' or S == '':
-#            return 0
-#        else:
-#            count = 0
-#            for p in J:
-#                for q in S:
-#                    if p == q:
-#                        count += 1
-#        return count
         if J == '' or S == '':
             return 0
-        else:
-            d = {}
-            count = 0
-            for p in S:
-                try:
-                    d[p] += 1
-                except:
-                    d[p] = 1
-            for q in J:
-                try:
-                    count += d[q]
-                except:
-                    pass
-        return count
         
+        J = set(J)
+        
+        count = 0
+        for s in S:
+            if s in J:
+                count += 1
+        return count
+
+
+
+
 solu = Solution()
 #J, S = 'aA', 'aAAbbbb'
 #J, S = 'z', 'ZZ'
