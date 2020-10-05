1a1f8c332d6f965d9248c1d1dd5de804aaf6eb4e
1050669722 <42562697+1050669722@users.noreply.github.com>
2020-10-05 16:58:03 +0800

    Create problem0018_test02.py

---
+++ Python/problem0018_test02.py
@@ -0,0 +1,34 @@
+from typing import *
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        if len(nums) < 4:
+            return []
+
+        nums.sort()
+        ans = []
+
+        for first in range(0, len(nums), 1):
+            if nums[first] == nums[first - 1] and first > 0:
+                continue
+
+            for second in range(first + 1, len(nums), 1):
+                if nums[second] == nums[second - 1] and second > first + 1:
+                    continue
+
+                fourth = len(nums) - 1
+
+                for third in range(second + 1, len(nums), 1):
+                    if nums[third] == nums[third - 1] and third > second + 1:
+                        continue
+
+                    while third < fourth and nums[first] + nums[second] + nums[third] + nums[fourth] > target:
+                        fourth -= 1
+
+                    if third == fourth:
+                        break
+                    if nums[first] + nums[second] + nums[third] + nums[fourth] < target:
+                        continue
+                    if nums[first] + nums[second] + nums[third] + nums[fourth] == target:
+                        ans.append([nums[first], nums[second], nums[third], nums[fourth]])
+
+        return ans
