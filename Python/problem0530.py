6a662ae0f88fea16506bf2ed54f8b02e268c2975
1050669722 <42562697+1050669722@users.noreply.github.com>
2020-10-12 10:52:50 +0800

    Update problem0530.py

--- Python/problem0530.py
+++ Python/problem0530.py
@@ -5,35 +5,58 @@
 #         self.left = None
 #         self.right = None
 
+# class Solution:
+#     def getMinimumDifference(self, root: TreeNode) -> int:
+
+#         ImportError
+#         ans = sys.maxsize
+
+#         nums = [] #如果是二叉搜索树，应该不需要这个额外的O(n)空间
+#         def worker(TreeNode):
+#             if not TreeNode:
+#                 return None
+#             else:
+#                 nums.append(TreeNode.val)
+            
+#             if TreeNode.left:
+#                 worker(TreeNode.left)
+            
+#             if TreeNode.right:
+#                 worker(TreeNode.right)
+
+#         worker(root)
+        
+#         nums.sort()
+
+#         for i, num in enumerate(nums):
+#             if i != 0:
+#                 ans = min(num - nums[i - 1], ans)
+#             # if i != len(nums) - 1:
+#                 # ans = min(nums[i + 1] - num, ans)
+
+#         return ans
+
+
 class Solution:
     def getMinimumDifference(self, root: TreeNode) -> int:
-
-        ImportError
+        import sys
         ans = sys.maxsize
+        pre = -sys.maxsize
 
-        nums = [] #如果是二叉搜索树，应该不需要这个额外的O(n)空间
-        def worker(TreeNode):
-            if not TreeNode:
+        def worker(root):
+            nonlocal ans, pre #这里如果换成global，就会提示下面的ans, pre没有定义
+            if not root:
                 return None
-            else:
-                nums.append(TreeNode.val)
             
-            if TreeNode.left:
-                worker(TreeNode.left)
-            
-            if TreeNode.right:
-                worker(TreeNode.right)
+            worker(root.left)
+
+            tmp = root.val - pre
+            ans = min(ans, tmp)
+            pre = root.val
+
+            worker(root.right)
 
         worker(root)
-        
-        nums.sort()
-
-        for i, num in enumerate(nums):
-            if i != 0:
-                ans = min(num - nums[i - 1], ans)
-            # if i != len(nums) - 1:
-                # ans = min(nums[i + 1] - num, ans)
 
         return ans
-
 
