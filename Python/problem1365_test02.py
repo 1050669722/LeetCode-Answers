186c3a2fccace77f623123beb4faa3ddd26f3a75
1050669722 <42562697+1050669722@users.noreply.github.com>
2020-10-26 09:20:36 +0800

    Update and rename problem_1365_test02.py to problem1365_test02.py

---
+++ Python/problem1365_test02.py
@@ -0,0 +1,38 @@
+# from collections import Counter, defaultdict
+
+# class Solution:
+#     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+#         count = Counter(nums)
+#         # print('count: {}'.format(count))
+        
+#         tmp = nums.copy() #list(set(nums.copy()))
+#         tmp.sort()
+#         # print('tmp: {}'.format(tmp))
+#         lastIdx = defaultdict(int)
+#         for i, num in enumerate(tmp):
+#             lastIdx[num] = i
+        
+#         smallerThanIt = {}
+#         for t in tmp:
+#             smallerThanIt[t] = lastIdx[t] - (count[t] - 1)
+
+#         ans = []
+#         for num in nums:
+#             ans.append(smallerThanIt[num])
+
+#         return ans
+
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        tmp = [0] * (max(nums) + 1)
+        for num in nums:
+            tmp[num] += 1
+        
+        ans = []
+        for num in nums:
+            ans.append(sum(tmp[:num]))
+        
+        return ans
+
+
