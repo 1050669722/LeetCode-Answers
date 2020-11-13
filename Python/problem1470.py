ccdc2774a187f7b4303736e17203d27103803708
1050669722 <42562697+1050669722@users.noreply.github.com>
2020-11-13 14:56:42 +0800

    Create problem1470.py

---
+++ Python/problem1470.py
@@ -0,0 +1,11 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        # ans = []
+        # for ele in zip(nums[:len(nums) // 2], nums[len(nums) // 2:]):
+        #     ans += ele
+        # return ans
+        
+        nums[::2],nums[1::2]=nums[:n],nums[n:]
+        return nums
+        
+        # 应该还有额外空间为O(1)的办法
