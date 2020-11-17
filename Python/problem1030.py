26fcff5c7cb6e2f6303c46eae600bad12ce79037
1050669722 <42562697+1050669722@users.noreply.github.com>
2020-11-17 09:34:11 +0800

    Update problem1030.py

--- Python/problem1030.py
+++ Python/problem1030.py
@@ -1,12 +1,36 @@
+# class Solution:
+#     def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int) -> List[List[int]]:
+#         r_idx_mat = list(range(C)) * R
+#         c_idx_mat = list(range(R)) * C
+#         for r, row in enumerate(r_idx_mat):
+
+
 class Solution:
     def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int) -> List[List[int]]:
-        r_idx_mat = list(range(C)) * R
-        c_idx_mat = list(range(R)) * C
-        for r, row in enumerate(r_idx_mat):
+        # # idxes = []
+        # # for r in range(R):
+        # #     for c in range(C):
+        # #         idxes.append([r, c])
+        # # idxes = [[r, c] for r in range(R) for c in range(C)]
+        # # idxes.sort(key=lambda x: abs(x[0]-r0) + abs(x[1]-c0))
+        # # return idxes
+        # # return sorted(idxes, key=lambda x: abs(x[0]-r0) + abs(x[1]-c0))
+        # return sorted([[r, c] for r in range(R) for c in range(C)], key=lambda x: abs(x[0]-r0) + abs(x[1]-c0))
 
+        maxDist = max(r0, R - 1 - r0) + max(c0, C - 1 - c0)
+        from collections import defaultdict
+        bucket = defaultdict(list)
+        dist = lambda r1, r2, c1, c2: abs(r1 - r2) + abs(c1 - c2)
 
-            
+        for r in range(R):
+            for c in range(C):
+                bucket[dist(r, r0, c, c0)].append([r, c])
 
+        ret = list()
+        for Dist in range(maxDist + 1):
+            ret.extend(bucket[Dist])
+
+        return ret
 
 
 
